6c03615c0de475f453ea593f4e0a1cd02f862d56
Yumechi <yume@yumechi.jp>
2026-09-26 13:34:08 +0800

    chore: cleanup poweroff.c

--- src/poweroff.c
+++ src/poweroff.c
@@ -1,12 +1,7 @@
 #include "poweroff.h"
 #include "leds.h"
-#include <ant_interface.h>
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/sys/poweroff.h>
 #include <zephyr/sys/reboot.h>
-
-LOG_MODULE_REGISTER(poweroff, LOG_LEVEL_INF);
 
 void enter_poweroff(void) {
   led_clear_bit(POWER_LED_BIT);
