f8794c263bbf0449ec0df214c6333944c4b6bc7a
Yumechi <yume@yumechi.jp>
2026-09-26 12:40:24 +0800

    enhance: more power consumption changes

--- src/main.c
+++ src/main.c
@@ -330,7 +330,7 @@ void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
       bt_le_scan_stop();
       int err = bt_conn_le_create(
           addr, CONN_CREATE_PARAMS,
-          BT_LE_CONN_PARAM(12, 32, 0, BT_GAP_MS_TO_CONN_TIMEOUT(2000)),
+          BT_LE_CONN_PARAM(80, 120, 0, BT_GAP_MS_TO_CONN_TIMEOUT(2000)),
           &central_profile_instances[i].conn);
 
       if (err) {
@@ -407,7 +407,7 @@ void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
       bt_le_scan_stop();
       int err = bt_conn_le_create(
           addr, CONN_CREATE_PARAMS,
-          BT_LE_CONN_PARAM(12, 32, 0, BT_GAP_MS_TO_CONN_TIMEOUT(2000)),
+          BT_LE_CONN_PARAM(80, 120, 0, BT_GAP_MS_TO_CONN_TIMEOUT(2000)),
           &central_profile_instances[i].conn);
 
       if (err) {
